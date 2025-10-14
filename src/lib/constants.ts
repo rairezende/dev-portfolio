991e5bc62fc976c1a23e71079575bc37de8506c4
Raiane <raiane.rze@gmail.com>
2025-10-14 16:59:41 -0300

    fixed import

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,12 +7,12 @@ export const loaderAnimation = [
 ]
 
 export const LINKS = {
-  github: 'https://github.com/bue221',
-  linkedin: 'https://www.linkedin.com/in/bue221/',
-  mail: 'mailto:camiloplaza3@gmail.com',
-  instagram: 'https://www.instagram.com/caj_ink/',
-  medium: 'https://ladvace.medium.com/',
-  discord: 'https://discordapp.com/users/163300027618295808',
+  github: 'https://github.com/rairezende',
+  linkedin: 'https://www.linkedin.com/in/raiane-rezende/',
+  mail: 'mailto:raiane.rze@gmail.com',
+  substack: 'https://7andar.substack.com/',
+  cva: 'https://www.figma.com/community/plugin/1263631854497422865/cva-component-variants-automator',
+  figmaSdk: 'https://github.com/figma-plugin-sdk/design-system',
 }
 
 // Global
@@ -30,10 +30,10 @@ export const WORK: Page = {
 }
 
 // Blog Page
-export const BLOG: Page = {
-  TITLE: 'Blog',
-  DESCRIPTION: 'Writing on topics I am passionate about.',
-}
+// export const BLOG: Page = {
+//   TITLE: 'Blog',
+//   DESCRIPTION: 'Writing on topics I am passionate about.',
+// }
 
 // Projects Page
 export const PROJECTS: Page = {
@@ -59,78 +59,23 @@ export const STUDIES = [
 
 export const EXPERIENCE = [
   {
-    company: 'Straico',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software Engineer',
+    company: 'Toki Labs',
+    position: 'Full Stack Software Developer',
     start: '2021',
-    link: 'https://straico.com/',
-    end: 'Current',
+    link: 'http://tokilabs.io',
+    end: '2025',
     tasks: [
-      'Integration IA services with react and tailwind css',
-      'Development and build of DB with mongoDB',
+      '',
     ],
   },
   {
-    company: 'Spot2',
-    location: 'Mexico City, Mexico',
-    position: 'Software Engineer',
-    link: 'https://spot2.mx/',
-    start: '2021',
-    end: 'Current',
-    tasks: [
-      'Development of the Spot2 platform with the use of React, Redux, and Material UI',
-      'Development map with the use of Google Maps API',
-      'Development internal platform with the use of React, Redux, and Material UI',
-      'Testing and debugging',
-    ],
-  },
-  {
-    company: 'Imaginamos',
-    link: 'https://imaginamos.com/',
-    location: 'Bogotá D C, Colombia',
-    position: 'Frontend developer',
-    start: '2021',
+    company: 'Toki Labs',
+    position: 'Programming Mentee',
+    link: 'http://tokilabs.io',
+    start: '2020',
     end: '2021',
     tasks: [
-      'Development of the Imaginamos platform with the use of React, Redux, and Material UI',
-      'Work in ETB project with the use of React, Redux, and Material UI',
-    ],
-  },
-  {
-    company: 'INETUM',
-    location: 'Bogotá D C, Colombia',
-    position: 'Frontend developer',
-    start: '2021',
-    link: 'https://www.inetum.com/es',
-    end: '2021',
-    tasks: [
-      'Support in the QA area and bug review',
-      'Use of SCRUM methodology',
-      'Claro projects with the use of frameworks and libraries such as react and angular',
-      'Use of redux toolkit as aproposal which allowed a shorter development time when using this tool',
-    ],
-  },
-  {
-    company: 'S I G',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software developer',
-    start: '2021',
-    end: '2021',
-    tasks: [
-      'Development of a dashboard with react admin and chartjs for managing and entering information',
-    ],
-  },
-  {
-    company: 'BOOKII',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software developer',
-    start: '2019',
-    end: '2021',
-    tasks: [
-      'Productive collaborationwiththebackendteamforthecreationofthe conversationalclubs',
-      'Effective coding of conversational clubs following design guide lines and using the Redux statemanager',
-      'Creation of the Bookii page in Spanish and change of texts in cms',
-      'Generation of static posts using Gatsby and the Contentful cms for the Bookii blog',
+      '',
     ],
   },
 ]
